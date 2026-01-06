dd1b3e107ec07828250920ab1ea7aff5ab4d1a41
Jens Engelke <jens.engelke@gmail.com>
2026-01-06 12:49:07 +0100

    refresh_token

--- collect-data.py
+++ collect-data.py
@@ -273,8 +273,13 @@ def main():
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
+                try:
+                    creds.refresh(Request())
+                except Exception as e:
+                    print(f"Error refreshing Google token: {e}")
+                    creds = None
+            
+            if not creds:
                 flow = InstalledAppFlow.from_client_secrets_file(".config.json", SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
